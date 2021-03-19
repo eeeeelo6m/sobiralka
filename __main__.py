35e8f7ba02f633be243c19cf69e67853be26741f
agd30 <agd30072010@gmail.com>
2021-03-19 16:44:52 +0300

    начатая версия 6

--- __main__.py
+++ __main__.py
@@ -1,15 +1,19 @@
 from pygame import draw, event, display, key
-import pygame, time, help
+import pygame, time, help,random
 pygame.init()
 TIMER_ID = event.custom_type()
 screen = display.set_mode([900, 700])
+obrabotca_block=pygame.image.load("picture/супер гер бой.jpg")
+obrabotca_block=help.izmeni_kartinku(obrabotca_block,70,70,[0,0,0],10)
 obrabotca_vistrel= pygame.image.load("picture/vistrel.png")
 obrabotca_vistrel=help.izmeni_kartinku(obrabotca_vistrel,40,110,[0,0,0],10)
 player = pygame.image.load("picture/player.jpg")
 player = help.izmeni_kartinku(player, 50, 150, [235, 28, 36], 100)
+block= pygame.Rect([random.randint(30,870),10,50,50])
 obekt = pygame.Rect([400, 550, 50, 150])
 vistrel_rect = []
 mogu_strelyt = True
+
 
 
 def draw_vistrel():
@@ -49,8 +53,10 @@ def obrabotka_event():
 def draw_screen():
     screen.fill([123, 0, 255])
     draw_player()
+    draw_block()
     draw_vistrel()
     display.flip()
+
 
 
 def draw_player():
@@ -76,13 +82,15 @@ def vistrel():
     global vistrel_rect, mogu_strelyt
 
     if mogu_strelyt == True:
-
         a = pygame.Rect([obekt.x + 20, obekt.top - 100, 10, 100])
         vistrel_rect.append(a)
         mogu_strelyt = False
-        pygame.time.set_timer(TIMER_ID,1000,1)
-    else:
-        print("стрелять нельзя")
+        pygame.time.set_timer(TIMER_ID, 1000, 1)
+
+
+def draw_block():
+    #draw.rect(screen,[0,0,0],block,5,1000000)
+    screen.blit(obrabotca_block,block)
 
 
 while 1 == 1:
@@ -90,4 +98,6 @@ while 1 == 1:
     obrabotka_event()
     attack_vistrel()
     ogranichnie()
+
     draw_screen()
+
